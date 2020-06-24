9d45b1f94cf4dd92f796c48f5cc131df0e37cf62
Kevin Mullins <kev@mullinsmail.com>
2020-06-24 14:00:00 -0700

    Modified the footer

--- hooks/footer-extras.php
+++ hooks/footer-extras.php
@@ -1,9 +1,11 @@
-<?php
-/*
-<div class="navbar-fixed-bottom hidden-print alert alert-info">
+<nav class="navbar navbar-default navbar-fixed-bottom">
+<div class="container">
+<p class="navbar-text"><small>
+						Powered by <a class="navbar-link" href="https://bigprof.com/appgini/" target="_blank">BigProf AppGini 5.82 </a>
+					</small>
 <?php
 date_default_timezone_set("America/Los_Angeles");
-echo "The time is " . date("h:i:sa");
-?>
+echo "-  The time is " . date("h:i:sa");
+?></p>
 </div>
-*/
+</nav>;
